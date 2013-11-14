e7de519faa33c304454c82d6ca40736c3bfe1a1b
Benjamin Kloster <benkloster@gmail.com>
2013-11-14 09:32:46 +0100

    Fix FindOpenAL
    The previous version was a mess. The new version is pretty much
    copied & pasted from FindVorbis.

--- cmake_modules/FindOpenAL.cmake
+++ cmake_modules/FindOpenAL.cmake
@@ -1,20 +1,31 @@
-# Locate OpenAL
-# This module defines
-# OPENAL_LIBRARY
-# OPENAL_FOUND, if false, do not try to link to OpenAL 
-# OPENAL_INCLUDE_DIR, where to find the headers
+# - Try to find OpenAL
+# Once done, this will define
+#
+#  OpenAL_FOUND - system has OpenAL
+#  OpenAL_INCLUDE_DIRS - the OpenAL include directories
+#  OpenAL_LIBRARIES - link these to use OpenAL
 
-if (OPENAL_LIBRARY AND OPENAL_INCLUDE_DIR)
-    # in cache already
-    set(OPENAL_FOUND TRUE)
-else (OPENAL_LIBRARY AND OPENAL_INCLUDE_DIR)
+include(LibFindMacros)
 
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(OpenAL_PKGCONF OpenAL)
 
-    find_path(OPENAL_INCLUDE_DIR 
-        NAMES al.h
-    )
+# Include dir
+find_path(OpenAL_INCLUDE_DIR
+    NAMES al.h
+    PATHS ${OpenAL_PKGCONF_INCLUDE_DIRS}
+    PATH_SUFFIXES AL
+)
 
-    find_library(OPENAL_LIBRARY 
-        NAMES  OpenAL al openal OpenAL32
-    )
-endif ()
+# OpenAL library
+find_library(OpenAL_LIBRARY
+    NAMES OpenAL al openal OpenAL32
+    PATHS ${OpenAL_PKGCONF_LIBRARY_DIRS}
+)
+
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(OpenAL_PROCESS_INCLUDES OpenAL_INCLUDE_DIR OpenAL_INCLUDE_DIRS)
+set(OpenAL_PROCESS_LIBS OpenAL_LIBRARY OpenAL_LIBRARIES)
+libfind_process(OpenAL)
+
